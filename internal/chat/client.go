11402e795b5e28eff9d199577f866251e93b3fd9
paulocesarvasco <paulocvasco.93@gmail.com>
2024-09-11 16:51:45 -0300

    Check write message error and use broadcast channel

--- internal/chat/client.go
+++ internal/chat/client.go
@@ -1,6 +1,8 @@
 package chat
 
 import (
+	"log"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -12,20 +14,31 @@ type Client struct {
 func (client *Client) ReadMessages(room *ChatRoom) {
 	defer func() {
 		room.Unregister <- client
-		client.Conn.Close()
+		client.Conn.Close() // Ensure the connection is properly closed
 	}()
+
 	for {
 		_, message, err := client.Conn.ReadMessage()
 		if err != nil {
+			// Log and handle the error
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Error reading message: %v", err)
+			}
 			break
 		}
-		room.BroadcastMessage(message)
+
+		// Broadcast the message to the chat room
+		room.Broadcast <- message
 	}
 }
 
 func (client *Client) WriteMessages() {
-	defer client.Conn.Close()
+	defer client.Conn.Close() // Ensure the connection is properly closed
+
 	for message := range client.Send {
-		client.Conn.WriteMessage(websocket.TextMessage, message)
+		if err := client.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			log.Printf("Error writing message: %v", err)
+			break
+		}
 	}
 }
