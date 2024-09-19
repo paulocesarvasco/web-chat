a9f774c39aef471fad53ab6bbe1633d09b9582f7
paulocesarvasco <paulocvasco.93@gmail.com>
2024-09-19 13:57:48 -0300

    Implement credential validator as a middleware

---
+++ chat/internal/auth/client.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+)
+
+func ValidateCredentials(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+			return
+		}
+
+		authReq, err := http.NewRequest(http.MethodPost, "http://auth_service:8081/login", nil)
+		if err != nil {
+			http.Error(w, "Failed to create request to auth_service", http.StatusInternalServerError)
+			return
+		}
+		authReq.Header.Set("Authorization", authHeader)
+
+		client := &http.Client{}
+		res, err := client.Do(authReq)
+		if err != nil {
+			log.Print(err)
+			http.Error(w, "Authentication failed", http.StatusInternalServerError)
+			return
+		}
+		if res.StatusCode != http.StatusOK {
+			http.Error(w, "Authentication failed", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
