cdb1a6fd239b2dcb8b08b73009d95ffc58adfcd4
paulocesarvasco <paulocvasco.93@gmail.com>
2024-09-19 13:58:42 -0300

    Use chi as router engine and set auth middleware

--- chat/cmd/api/main.go
+++ chat/cmd/api/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"github.com/paulocesarvasco/web-chat/chat/internal/chat"
-	"github.com/paulocesarvasco/web-chat/chat/internal/ws"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/paulocesarvasco/web-chat/chat/internal/auth"
+	"github.com/paulocesarvasco/web-chat/chat/internal/chat"
+	"github.com/paulocesarvasco/web-chat/chat/internal/ws"
 )
 
 func main() {
 	room := chat.NewChatRoom()
 	go room.Run()
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	r := chi.NewMux()
 
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		ws.HandleConnections(w, r, room)
-	})
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/", fs)
+	r.HandleFunc("/chat", auth.ValidateCredentials(
+		func(w http.ResponseWriter, r *http.Request) {
+			ws.HandleConnections(w, r, room)
+		}))
 
 	log.Println("Chat server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
