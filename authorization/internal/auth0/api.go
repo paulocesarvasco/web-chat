6046f100fda70b0c5c968913363ebcce68ca0a6f
paulocesarvasco <paulocvasco.93@gmail.com>
2024-09-25 11:01:25 -0300

    Implement api client to request access token

---
+++ authorization/internal/auth0/api.go
@@ -0,0 +1,46 @@
+package auth0
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/paulocesarvasco/web-chat/authorization/internal/requests"
+)
+
+type API interface {
+	RequestAccessToken(ctx context.Context) (string, error)
+}
+
+type api struct{}
+
+func (a *api) RequestAccessToken(ctx context.Context) (string, error) {
+	url := fmt.Sprintf("https://%s/oauth/token", os.Getenv("AUTH0_DOMAIN"))
+	requestPayload := AccessTokenRequestPayload{
+		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		Audience:     os.Getenv("AUTH0_AUDIENCE"),
+		GrantType:    "client_credentials",
+	}
+	req, err := requests.NewPostRequest(ctx, url, requestPayload)
+	if err != nil {
+		return "", err
+	}
+	req.AddContentTypeJSON()
+	code, res := req.Execute()
+	if code != http.StatusOK {
+		return "", fmt.Errorf("failed to get token: %s", res)
+	}
+	var tokenResponse AccessTokenResponsePayload
+	err = json.Unmarshal(res, &tokenResponse)
+	if err != nil {
+		return "", err
+	}
+	return tokenResponse.AccessToken, nil
+}
+
+func New() API {
+	return &api{}
+}
