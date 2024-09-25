7b71c62134a52aa63f6b5a0174de2fe50bfb45cc
paulocesarvasco <paulocvasco.93@gmail.com>
2024-09-25 12:59:46 -0300

    Implement token validation using auth service

--- chat/internal/auth/client.go
+++ chat/internal/auth/client.go
@@ -8,6 +8,25 @@ import (
 func ValidateCredentials(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if token := r.URL.Query().Get("token"); token != "" {
+			log.Print("validating token")
+			checkReq, err := http.NewRequestWithContext(r.Context(), http.MethodGet, "http://auth_service:8081/validate", nil)
+			if err != nil {
+				http.Error(w, "Failed to create request to auth_service", http.StatusInternalServerError)
+				return
+			}
+			checkReq.Header.Set("token", r.Header.Get("token"))
+			client := &http.Client{}
+			res, err := client.Do(checkReq)
+			if err != nil {
+				log.Print(err)
+				http.Error(w, "request verify token failed", http.StatusInternalServerError)
+				return
+			}
+			if res.StatusCode != http.StatusOK {
+				http.Error(w, "token validation failed", res.StatusCode)
+				return
+			}
+			log.Print("token authorized")
 			next(w, r)
 		}
 
